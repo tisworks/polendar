66a25fd4114b7041e48fe3f8625d76a5949e39d1
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2020-03-28 22:48:26 -0300

    project start

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('database.db');
+
+db.serialize(function() {
+    db.run("CREATE TABLE if not exists lorem (info TEXT)");
+
+    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (var i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+        console.log(row.id + ": " + row.info);
+    });
+});
+
+db.close();
+
---
+++ start.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('src/index.html')
+}
+
+app.whenReady().then(createWindow);
